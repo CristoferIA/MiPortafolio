c8ff14ca2c8a7410c1f7b6556e1d6a93aed4c866
cristoferia <crostofer_221@hotmail.com>
2023-03-29 15:06:51 -0500

    mi-portafolio-v3.9.6

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,13 +11,13 @@ const About = () => {
           <h1>Soy Cristofer Islachín, Desarrollador Full Stack Developer</h1>
           <p>
             {" "}
-            Soy Ingeniero de Sistemas con +1 año de experiencia, me desempeño
-            como desarrollador en frontend y backend. Busco trabajo como
-            developer en una empresa que me permita explotar mis conocimientos
-            de JavaScript, React.js, Express, HTML, CSS, GIT, SQL, MySQL,
-            PostgreSQL, PHP, Python, Java, Oracle y todo lo relacionado a la
-            programación. Dentro de mis habilidades está la investigación y el
-            trabajo en equipo. Mi área de interés es el data science.
+            Soy Ingeniero de Sistemas con más de 1 año de experiencia, me
+            desempeño como desarrollador en frontend y backend. Busco trabajo
+            como developer en una empresa que me permita explotar mis
+            conocimientos de JavaScript, React.js, Express, HTML, CSS, GIT, SQL,
+            MySQL, PostgreSQL, PHP, Python, Java, Oracle y todo lo relacionado a
+            la programación. Dentro de mis habilidades está la investigación y
+            el trabajo en equipo. Mi área de interés es el data science.
           </p>
           <p>
             Adicionalmente tengo estudio técnico en operación de computadoras,
